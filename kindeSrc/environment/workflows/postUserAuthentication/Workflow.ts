3029c168d53fa47fa1e7b0dae434ebc4d28100c3
Rai Raman <38846786+rairaman@users.noreply.github.com>
2025-09-05 16:20:27 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -6,11 +6,47 @@ import {
 
 // The setting for this workflow
 export const workflowSettings: WorkflowSettings = {
-  id: "onPostUserAuthentication",
-  trigger: WorkflowTrigger.PostAuthentication,
+  id: "nonPersistentSessionWorkflow",
+  name: "Non Persistent Session Workflow",
+  trigger: WorkflowTrigger.UserTokenGeneration,
+  bindings: {
+    "kinde.accessToken": {},
+    "kinde.ssoSession": {},
+    "kinde.env": {},
+  },
+  failurePolicy: { action: "stop" }
 };
 
 // The workflow code to be executed when the event is triggered
 export default async function Workflow(event: onPostAuthenticationEvent) {
-  console.log("Hello world");
+  NonPersistentSessionWorkflow(event);
+}
+
+function c() {
+  if (!kinde.accessToken)
+    throw new Error(
+      "accessToken binding not available, please add to workflow/page settings to enable"
+    );
+  let e2 = kinde.accessToken.getCustomClaims();
+  return new Proxy(e2, r);
+}
+
+async function NonPersistentSessionWorkflow(event) {
+  let policy = "persistent"
+
+  const nonPersistentConnectionIDs = getEnvironmentVariable(
+    "NON_PERSISTENT_SESSION_CONNECTION_IDS"
+  ).value.split(",");
+  
+  console.log("nonPersistentConnectionIDs:", nonPersistentConnectionIDs)
+
+  kinde.ssoSession.setPolicy(policy)
+  // check if the connection id is in the non persistent connection ids
+  if (nonPersistentConnectionIDs.includes(event.context.auth.connectionId)) {
+    kinde.ssoSession.setPolicy("non_persistent");
+    console.log("setting sso session policy to non_persistent")
+  }
+
+  let accessToken = c();
+  (accessToken.isDeployed = !0), (accessToken.ksp = policy);
 }
